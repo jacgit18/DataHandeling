a347ac392f98b58e97b0fdf9b96e4166f3428773
jac <joshuacarpentier21@gmail.com>
2019-12-13 23:57:50 -0500

    add

--- DataHandeling/src/edu/cityech/cst/review/stock/StockDAO.java
+++ DataHandeling/src/edu/cityech/cst/review/stock/StockDAO.java
@@ -10,11 +10,10 @@ import edu.cityech.cst.review.model.Stock;
 public class StockDAO {
 
 	public List<Stock> findAll() {
-		Map<String, Float> map = new HashMap<>();
-//		map.put("", 36.13);
 		
-		List<String> bankStocks =
-				Arrays.asList("CM:36.13","C:77.10","HSBC:38.45","RY:79.26","TD:55.63");
+		
+//		List<Stock> bankStocks =
+//				Arrays.asList("CM:36.13","C:77.10","HSBC:38.45","RY:79.26","TD:55.63");
 		
 		return this.findAll(); 
 	}
